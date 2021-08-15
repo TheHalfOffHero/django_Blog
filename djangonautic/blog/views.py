b398cec87f67beedb9bb4e4978a976afc7e38d11
mruiz <mfruiz33@gmail.com>
2021-08-15 16:31:23 -0400

    working with html templates

--- djangonautic/blog/views.py
+++ djangonautic/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+#Test data
+posts=[
+    {
+        'author': 'Matthew R',
+        'title': 'Blog Post 1',
+        'content': 'first post content',
+        'date_posted': 'August 27th, 2021'
+    },
+    {
+        'author': 'Matthew R',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'June 8th, 2021'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context ={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
